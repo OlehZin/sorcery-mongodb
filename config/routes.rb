d1c9b1354c88411348a6467ac197eb6e03b11902
OlehZin <biosoln91@gmail.com>
2020-11-06 14:43:57 +0200

    design_notice fixes and routes fixes

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root :to => "home#index"
-  resources :articles do
-    get :title_image, on: :member
-  end
+  resources :articles
   post '/articles/new', to: 'articles#create', as: :articles_new
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create] do
+    member do
+      get :activate
+    end
+  end
   resources :sessions, only: [:create, :destroy]
   delete '/log_out', to: 'sessions#destroy', as: :log_out
   get "login" => "sessions#new",  as: "login"
